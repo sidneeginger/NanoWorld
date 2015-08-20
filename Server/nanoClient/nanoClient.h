dac40fd940f3700b5f7569173a1919c93b6510db
JiangJun <jiang.jun@microcyber.cn>
2015-08-20 09:40:39 +0800

    init

---
+++ Server/nanoClient/nanoClient.h
@@ -0,0 +1,32 @@
+
+// nanoClient.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CnanoClientApp: 
+// 有关此类的实现，请参阅 nanoClient.cpp
+//
+
+class CnanoClientApp : public CWinApp
+{
+public:
+	CnanoClientApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CnanoClientApp theApp;
