d3be077ae5958ce1be4d6a0ee010121d13f0837a
Jun Chiang <smallsmilejiang@msn.com>
2015-11-05 09:10:07 +0800

    updater version

--- Server/revision_data.h
+++ Server/revision_data.h
@@ -3,14 +3,14 @@
  #define _HASH                      "f7dba226fea3"
  #define _DATE                      "2015-10-26 17:06:30 +0100"
  #define _BRANCH                    "6.x"
- #define _SOURCE_DIRECTORY          "E:/TornadoProject/TrinityCore"
+ #define _SOURCE_DIRECTORY          "E:/TornadoProject/Tornado"
  #define _MYSQL_EXECUTABLE          "C:/Program Files/MySQL/MySQL Server 5.6/bin/mysql.exe"
- #define _FULL_DATABASE             "TDB_full_world_6.02_2015_07_14.sql"
- #define _HOTFIXES_DATABASE         "TDB_full_hotfixes_6.02_2015_07_14.sql"
- #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2015 TrinityCore"
+ #define _FULL_DATABASE             "TDB_full_world_.sql"
+ #define _HOTFIXES_DATABASE         "TDB_full_hotfixes_.sql"
+ #define VER_COMPANYNAME_STR        "Tornado Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2015 Tornado"
  #define VER_FILEVERSION            0,0,0
- #define VER_FILEVERSION_STR        "f7dba226fea3 2015-10-26 17:06:30 +0100 (6.x branch)"
+ #define VER_FILEVERSION_STR        "f7dba226fea3 2015-10-26 17:06:30 +0100 (develop branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
  #define COMPILER_C_FLAGS           " /DWIN32 /D_WINDOWS /W3"
